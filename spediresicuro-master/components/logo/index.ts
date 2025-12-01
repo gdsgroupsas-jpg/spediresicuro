867d4c22a8e5120bac67ddb24fd245ca94da2568
gdsgroupsas-jpg <gdsgroupsas@gmail.com>
2025-12-01 19:43:19 +0100

    chore: Rimossa cartella duplicata spediresicuro-master e aggiunta a .gitignore

--- spediresicuro-master/components/logo/index.ts
+++
@@ -1,7 +0,0 @@
-// Export centralizzato di tutti i componenti logo
-export { default as LogoHorizontal } from './logo-horizontal'
-export { default as LogoIcon } from './logo-icon'
-export { default as LogoStacked } from './logo-stacked'
-export { default as LogoBlack } from './logo-black'
-export { default as LogoWhite } from './logo-white'
-
