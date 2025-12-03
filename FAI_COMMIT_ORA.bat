33290b13ac991a393a5f43124c3e0fef5c064dc6
gdsgroupsas-jpg <sigorn@hotmail.it>
2025-12-03 15:00:28 +0100

    fix: Correzione TypeScript session role + tutti i fix completati

---
+++ FAI_COMMIT_ORA.bat
@@ -0,0 +1,9 @@
+@echo off
+echo COMMIT AUTOMATICO
+cd /d C:\spediresicuro-master\spediresicuro
+git config --global core.pager ""
+git add -A
+git commit -m "fix: Correzione TypeScript session role + tutti i fix completati"
+git push
+echo FATTO!
+
